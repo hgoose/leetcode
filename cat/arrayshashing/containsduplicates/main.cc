006421623163c265214d6220b8fa6e3aa6f8a2d1
hgoose <noodlecloudcode@gmail.com>
2025-01-24 10:01:00 -0600

    first

---
+++ cat/arrayshashing/containsduplicates/main.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using std::cout;
+using std::vector;
+using std::endl;
+
+int main(int argc, char** argv) {
+
+    cout << "Hello world" << endl;
+
+    return 0;
+}
