0b83afd4e9b9655f07151086111683c3ccc1fcd0
hgoose <noodlecloudcode@gmail.com>
2025-03-05 13:12:09 -0600

    Commit

--- dailies/counttotalnumberofcoloredcells/main.cc
+++ dailies/counttotalnumberofcoloredcells/main.cc
@@ -44,8 +44,8 @@ using lims = std::numeric_limits<T>;
 
 class Solution {
 public:
-    vector<string> generateParenthesis(int n) {
-
+    long long coloredCells(int n) {
+        return 1 + 4 * (((n-1)*n)/2);
     }
 };
 
