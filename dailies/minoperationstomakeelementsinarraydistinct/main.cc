aa33035260440052dc761627b370561764530b68
hgoose <noodlecloudcode@gmail.com>
2025-04-09 16:36:20 -0500

    Commit

---
+++ dailies/minoperationstomakeelementsinarraydistinct/main.cc
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <queue>
+#include <utility>
+#include <cmath>
+#include <bitset>
+#include <array>
+#include <map>
+#include <limits>
+#include <numeric> // std::accumulate
+#include <climits> // INT_MAX, INT_MIN, ...
+#include <tuple>
+#include <unordered_set>
+#include <algorithm>
+#include <stack>
+#include <ranges>
+using std::accumulate;
+using std::pair;
+using std::stack;
+using std::unordered_set;
+using std::tuple;
+using std::map;
+using std::array;
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+using std::unordered_map;
+using std::queue;
+using std::priority_queue;
+using std::bitset;
+
+typedef long long dword;
+
+template<typename T>
+using pset = unordered_set<T>;
+
+template<typename T, typename U, typename W=std::less<T>>
+using pq = priority_queue<T,U,W>;
+
+template<typename T>
+using lims = std::numeric_limits<T>;
+
+template<typename T, typename U>
+using pmap = unordered_map<T,U>;
+
+typedef vector<vector<int>> VII;
+typedef vector<int> VI;
+struct TreeNode;
+struct ListNode;
+typedef ListNode ln;
+typedef TreeNode tn;
+#define PB push_back
+
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        bitset<101> bs;
+        for (int i=nums.size()-1; i>=0; --i) {
+            if (bs[i]) {
+               return (i+1)/3;  
+            }
+            bs[nums[i]] = 1;
+        }
+        return 0;
+    }
+};
+
+int main(int argc, char** argv) {
+    Solution s;
+    vector<int> v{1,2,3,4,2,3,3,5,7};
+
+    cout << s.minimumOperations(v);
+
+    return 0;
+}
