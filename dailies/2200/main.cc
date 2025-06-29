a9d097d2abd5bca4160d7bd0369f77bfc567294d
hgoose <noodlecloudcode@gmail.com>
2025-06-29 17:59:38 -0500

    Commit

---
+++ dailies/2200/main.cc
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <queue>
+#include <utility>
+#include <cmath>
+#include <bitset>
+#include <array>
+#include <map>
+#include <limits>
+#include <numeric> // std::accumulate
+#include <climits> // INT_MAX, INT_MIN, ...
+#include <tuple>
+#include <unordered_set>
+#include <algorithm>
+#include <stack>
+#include <ranges>
+using std::accumulate;
+using std::pair;
+using std::stack;
+using std::unordered_set;
+using std::tuple;
+using std::map;
+using std::array;
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+using std::unordered_map;
+using std::queue;
+using std::priority_queue;
+using std::bitset;
+
+typedef long long dword;
+
+
+template<typename T, typename U, typename W=std::less<T>>
+using pq = priority_queue<T,U,W>;
+
+typedef priority_queue<int, vector<int>, std::greater<int>> I_MIN_HEAP;
+typedef priority_queue<int, vector<int>> I_MAX_HEAP;
+
+template<typename T>
+using lims = std::numeric_limits<T>;
+
+template<typename T, typename U>
+using pmap = unordered_map<T,U>;
+
+typedef vector<vector<int>> VII;
+typedef vector<int> VI;
+struct TreeNode;
+struct ListNode;
+typedef ListNode ln;
+typedef TreeNode tn;
+#define PB push_back
+
+template<typename T>
+using pset = unordered_set<T>;
+
+class Solution {
+public:
+    vector<int> findKDistantIndices(vector<int>& nums, int key, int k) {
+        vector<int> res;
+        pset<int> seen;
+        for (int i=0; i<(int)nums.size(); ++i) {
+            if (nums[i] == key) {
+                int lb = -k + i, rb = k + i;
+                for (int j = lb; j<=rb && j < nums.size(); ++j) {
+                    if (seen.find(j) == seen.end()) {
+                        res.push_back(j);
+                        seen.insert(j);
+                    }
+                }
+            }
+        }        
+        return res;
+    }
+};
+
+int main(int argc, char** argv) {
+
+    return 0;
+}
